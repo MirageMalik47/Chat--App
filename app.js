6279f7502070c11c25aaa4785d7c1a59dccac604
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-17 22:21:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App setup
+var app = express();
+
+//static files
+app.use(express.static('public'));
+
+var server = app.listen('3000');
+console.log('Server Start on Port 3000');
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection',function(socket){
+    console.log('made socket connection',socket.id);
+
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data);
+    });
+
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data);
+    });
+});
